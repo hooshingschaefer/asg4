b662b8c8406a1f4567e0eea115e8456ae5ed62ea
hschaefe <hschaefe>
2019-05-19 12:46:00 -0700

    dkf

---
+++ applications/hw4/models/tables.py
@@ -0,0 +1,17 @@
+# Define your tables below (or better in another model file) for example
+#
+# >>> db.define_table('mytable', Field('myfield', 'string'))
+#
+# Fields can be 'string','text','password','integer','double','boolean'
+#       'date','time','datetime','blob','upload', 'reference TABLENAME'
+# There is an implicit 'id integer autoincrement' field
+# Consult manual for more options, validators, etc.
+
+
+db.define_table('r_products', Field('f_name', label='name'), 
+	Field('f_description', label='description'), Field('f_price', label='price', type='double'))
+
+db.r_products.id.readable = False
+
+# after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
